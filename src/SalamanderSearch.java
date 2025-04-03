6e12bd6e754b2aa955389917c2ba336eb6925b2e
Auberon López <auberonedu@gmail.com>
2025-04-03 08:05:43 -0700

    Added SalamanderSearch skeleton

---
+++ src/SalamanderSearch.java
@@ -0,0 +1,45 @@
+public class SalamanderSearch {
+    public static void main(String[] args) {
+        char[][] enclosure1 = {
+            {'.','.','.','.','.','.'},
+            {'W','.','W','W','.','.'},
+            {'.','.','W','.','.','W'},
+            {'f','W','.','.','W','.'},
+            {'W','.','W','s','.','.'},
+        };
+
+        char[][] enclosure2 = {
+            {'.','.','.','.','.','.'},
+            {'W','W','W','W','s','.'},
+            {'.','.','W','.','.','W'},
+            {'f','W','.','.','W','.'},
+            {'W','.','W','.','.','.'},
+        };
+    }
+
+    /**
+     * Returns whether a salamander can reach the food in an enclosure.
+     * 
+     * The enclosure is represented by a rectangular char[][] that contains
+     * ONLY the following characters:
+     * 
+     * 's': represents the starting location of the salamander
+     * 'f': represents the location of the food
+     * 'W': represents a wall
+     * '.': represents an empty space the salamander can walk through
+     * 
+     * The salamander can move one square at a time: up, down, left, or right.
+     * It CANNOT move diagonally.
+     * It CANNOT move off the edge of the enclosure.
+     * It CANNOT move onto a wall.
+     * 
+     * This method should return true if there is any sequence of steps that
+     * the salamander could take to reach food.
+     * 
+     * @param enclosure
+     * @return whether the salamander can reach the food
+     */
+    public boolean canReach(char[][] enclosure) {
+        return false;
+    }
+}
